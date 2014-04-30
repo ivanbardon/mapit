d86a763ab7375b90e2ed9dd2d06aa20830622f01
Ivan Bardon <ivanbardon@gmail.com>
2014-04-30 02:21:31 +0200

    Add buttons

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,19 @@
 var map;
+var mapaNormal = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}');
 var mapaNegro = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png');
-var mapaNormal = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'});
+var mapaGris = L.tileLayer('http://openmapsurfer.uni-hd.de/tiles/roadsg/x={x}&y={y}&z={z}');
+var mapaTopo =  L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}');
+
 function init(){
 	map = L.map('mapa', {
 	    center: [32.101189, 13.007812],
-	    zoom: 2,
+	    minZoom: 2,
+	    maxZoom:11,
+	    zoom: 11,
 	    scrollWheelZoom: false
 	})
 
-	map.addLayer(mapaNegro);
+	map.addLayer(mapaNormal);
 	map.locate({
 		watch: true,
 		enableHighAccuracy: true,

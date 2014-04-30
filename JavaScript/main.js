89b61c1351486e39e2aedb5aca82c8d12e7fa6dc
Ivan Bardon <ivanbardon@gmail.com>
2014-04-30 02:57:17 +0200

    Added .onLocationFound

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,13 +2,13 @@ var map;
 var mapaNormal = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 var mapaNegro = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png');
 var mapaGris = L.tileLayer('http://openmapsurfer.uni-hd.de/tiles/roadsg/x={x}&y={y}&z={z}');
-var mapaTopo =  L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}');
+var mapaTopo =  L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png');
 
 function init(){
 	map = L.map('mapa', {
 	    center: [32.101189, 13.007812],
 	    minZoom: 1,
-	    maxZoom:17,
+	    maxZoom:19,
 	    zoom: 2,
 	    scrollWheelZoom: false
 	})
@@ -19,8 +19,10 @@ function init(){
 		enableHighAccuracy: true,
 		setView: true
 	})
-	map.on('dragstart', function(){
-		map.stopLocate();
+	map.on('locationfound', function(){
+			map.on('dragstart', function(){
+			map.stopLocate();
+		})
 	})
 }
 
